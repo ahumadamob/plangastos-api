1ae1446c60bfc7e77ddd4d0796172f17ff1312ee
Mario Ahumada <ahumadamob@gmail.com>
2025-12-14 15:08:47 -0300

    First commit

---
+++ src/main/java/com/ahumadamob/plangastos_api/PlangastosApiApplication.java
@@ -0,0 +1,13 @@
+package com.ahumadamob.plangastos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlangastosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlangastosApiApplication.class, args);
+	}
+
+}
