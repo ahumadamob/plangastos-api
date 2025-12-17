f9ac8f7c9de69c99819086d031528ef9ca6cbaae
Mario Ahumada <ahumadamob@gmail.com>
2025-12-17 13:15:18 -0300

    Simplify Rubro request payload

--- src/main/java/io/github/ahumadamob/plangastos/dto/RubroRequestDto.java
+++ src/main/java/io/github/ahumadamob/plangastos/dto/RubroRequestDto.java
@@ -1,42 +1,32 @@
 package io.github.ahumadamob.plangastos.dto;
+
 public class RubroRequestDto {
 
-    private Long usuario_id;
     private Long naturalezaMovimiento_id;
     private String nombre;
-    private Long rubro_id;
     private Boolean activo;
-    
-	public Long getUsuario_id() {
-		return usuario_id;
-	}
-	public void setUsuario_id(Long usuario_id) {
-		this.usuario_id = usuario_id;
-	}
-	public Long getNaturalezaMovimiento_id() {
-		return naturalezaMovimiento_id;
-	}
-	public void setNaturalezaMovimiento_id(Long naturalezaMovimiento_id) {
-		this.naturalezaMovimiento_id = naturalezaMovimiento_id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public Long getRubro_id() {
-		return rubro_id;
-	}
-	public void setRubro_id(Long rubro_id) {
-		this.rubro_id = rubro_id;
-	}
-	public Boolean getActivo() {
-		return activo;
-	}
-	public void setActivo(Boolean activo) {
-		this.activo = activo;
-	}
 
+    public Long getNaturalezaMovimiento_id() {
+        return naturalezaMovimiento_id;
+    }
 
+    public void setNaturalezaMovimiento_id(Long naturalezaMovimiento_id) {
+        this.naturalezaMovimiento_id = naturalezaMovimiento_id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
 }
