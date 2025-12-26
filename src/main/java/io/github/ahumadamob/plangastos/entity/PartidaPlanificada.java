0052be63efedcc64684d8e28f0d24ff8fc43d7f0
Mario Ahumada <ahumadamob@gmail.com>
2025-12-26 14:44:41 -0300

    Omitir transacciones en partidas planificadas

--- src/main/java/io/github/ahumadamob/plangastos/entity/PartidaPlanificada.java
+++ src/main/java/io/github/ahumadamob/plangastos/entity/PartidaPlanificada.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -22,6 +24,7 @@ public class PartidaPlanificada extends RegistroPresupuesto {
 
     private LocalDate fechaObjetivo;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "partidaPlanificada", fetch = FetchType.LAZY)
     private List<Transaccion> transacciones = new ArrayList<>();
 
