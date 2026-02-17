1a3503edc27078571cee80407c349510be86c1db
Mario Ahumada <ahumadamob@gmail.com>
2026-02-17 18:46:01 -0300

    Fix integration test MockMvc setup without Boot test autoconfigure

--- src/test/java/io/github/ahumadamob/plangastos/integration/AuthAndOwnershipIntegrationTest.java
+++ src/test/java/io/github/ahumadamob/plangastos/integration/AuthAndOwnershipIntegrationTest.java
@@ -22,7 +22,6 @@ import javax.crypto.SecretKey;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -30,14 +29,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 @ActiveProfiles("test")
 class AuthAndOwnershipIntegrationTest {
 
-    @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -56,6 +58,7 @@ class AuthAndOwnershipIntegrationTest {
 
     @BeforeEach
     void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
         presupuestoRepository.deleteAll();
         usuarioRepository.deleteAll();
     }
